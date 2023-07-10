425a4b99a5c169e1603ab184611227a6c31efacc
XDzZyq <xiaodouzizyq@gmail.com>
2023-07-10 17:27:36 +0800

    7.10 using relative path

--- Cpp2Py/src/core/Cpp2Py.cpp
+++ Cpp2Py/src/core/Cpp2Py.cpp
@@ -12,19 +12,36 @@ int main()
 {
 	cout << "Hello CMake.\n" << endl;
 
-
+#if 1
 	pybind11::scoped_interpreter guard{};
 
-	//pybind11::module_ sys = pybind11::module_::import("sys");
-	
+	pybind11::print("Hello Python.\n");
+
 	pybind11::exec("import sys, os");
 	pybind11::exec("sys.path.append(os.getcwd())");
-	
+
 	pybind11::exec("import TestModule");
 	pybind11::exec("TestModule.info()");
 	pybind11::exec("test = TestModule.Test(2, 3)");
 	pybind11::exec("test.info()");
-	pybind11::exec("test.print()");
+	pybind11::exec("TestModule.Test.print()");
+#else
+	Py_Initialize();
+	if (!Py_IsInitialized())
+	{
+		cout << "Failed\n" << endl;
+		return 0;
+	}
+
+	PyRun_SimpleString("import sys, os");
+	PyRun_SimpleString("sys.path.append(os.getcwd())");
+
+	PyRun_SimpleString("import TestModule");
+	PyRun_SimpleString("TestModule.info()");
+	PyRun_SimpleString("test = TestModule.Test(2, 3)");
+	PyRun_SimpleString("test.info()");
+	PyRun_SimpleString("TestModule.Test.print()");
+#endif
 
 	std::cin.get();
 }
